744fd54275f67db605347faf588f69d4e39f2254
thais <thais.abreuoliveira@hotmail.com>
2021-07-09 17:06:25 -0300

    subir solução

---
+++ Model/ApiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeirAPI.Model
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
